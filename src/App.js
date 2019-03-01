d362c84b1d20e17d587b0616eeaedf74b473a606
mazahir89 <mazahir89@gmail.com>
2019-03-01 08:51:00 +0100

    created EstimatedTotal component and set it under a row

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Container } from "react-bootstrap";
 import SubTotal from "./components/Subtotal/SubTotal";
 import PickupSavings from "./components/PickupSavings/PickupSavings";
 import TaxesFees from "./components/TaxesFees/TaxesFees";
+import EstimatedTotal from "./components/EstimatedTotal/EstimatedTotal";
 import "./App.css";
 
 class App extends Component {
@@ -12,7 +13,8 @@ class App extends Component {
     this.state = {
       total: 100,
       pickupSavings: -3.85,
-      taxes: 0
+      taxes: 0,
+      estimatedTotal: 0
     };
   }
   render() {
@@ -22,6 +24,8 @@ class App extends Component {
           <SubTotal price={this.state.total.toFixed(2)} />
           <PickupSavings price={this.state.pickupSavings} />
           <TaxesFees taxes={this.state.taxes.toFixed(2)} />
+          <hr />
+          <EstimatedTotal price={this.state.estimatedTotal.toFixed(2)} />
         </Container>
       </div>
     );
---
+++ src/components/EstimatedTotal/EstimatedTotal.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import { Row, Col } from "react-bootstrap";
+
+export default class EstimatedTotal extends Component {
+  render() {
+    return (
+      <Row>
+        <Col md={6}>
+          <h2>Est. Total</h2>
+        </Col>
+        <Col md={6}>
+          <h2> {`$${this.props.price}`} </h2>
+        </Col>
+      </Row>
+    );
+  }
+}
