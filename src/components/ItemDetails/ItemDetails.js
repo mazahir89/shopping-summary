f3289af912bddf773c2baea2481645b1ac735fb6
mazahir89 <mazahir89@gmail.com>
2019-03-01 11:52:06 +0100

    image of items

--- src/components/ItemDetails/ItemDetails.js
+++ src/components/ItemDetails/ItemDetails.js
@@ -19,6 +19,18 @@ export default class ItemDetails extends Component {
           {this.state.open === false ? `See` : `Hide `} item details
           {this.state.open === false ? ` +` : ` -`}
         </Button>
+        <Collapse in={this.state.open}>
+          <div>
+            <Media>
+              <img
+                width={100}
+                height={100}
+                alt="thumbnail"
+                src="https://i5.walmartimages.com/asr/9898814f-8aea-47d7-a599-d80d0ba11781_1.308a8bc87d4ec569fd212318bfcb7aae.jpeg?odnWidth=undefined&odnHeight=undefined&odnBg=ffffff"
+              />
+            </Media>
+          </div>
+        </Collapse>
       </div>
     );
   }
